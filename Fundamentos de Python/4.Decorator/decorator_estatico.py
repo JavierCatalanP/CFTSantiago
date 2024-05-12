8ffd94f1b459a30765f071894ed3ff9658971397
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-05-11 20:34:36 -0400

    4. Decorator estatico en Python

---
+++ Fundamentos de Python/4.Decorator/decorator_estatico.py
@@ -0,0 +1,24 @@
+#def saludo():
+  #  print("Hola")
+    
+#objetoS = saludo #Estoy convertiendo la función en un objeto llamado "objetoS"
+
+#objetoS()
+
+def funcion_principal(funcion_parametro):
+    print("Dentro de funcion principal")
+    
+    def funcion_ejecutar():
+        print("Dentro de funcion ejecutar")
+        funcion_parametro()
+        
+    return funcion_ejecutar #Hasta aqui es un Decorator, nos puede servir para cuando tengamos que ejecutar muchas funciones 25
+    
+def saludo():
+    print("Hola")
+    
+def pregunta():
+    print("Como estas?")
+    
+respuesta = funcion_principal(pregunta)
+respuesta()
